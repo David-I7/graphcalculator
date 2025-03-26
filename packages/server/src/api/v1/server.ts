6f23b5e3dc4ef9be1e8f4ad9a9465696b2ba8ccc
David <daviodiosub09@gmail.com>
2025-03-26 13:11:41 +0200

    no message

--- packages/server/src/api/v1/server.ts
+++ packages/server/src/api/v1/server.ts
@@ -1,13 +1,38 @@
 import express from "express";
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  console.log(import.meta.url);
-  res.send("x");
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+}, serveStaticGZIP);
+
+app.use(
+  express.static(
+    path.join(fileURLToPath(import.meta.url), "../../../../../client/dist"),
+    {
+      setHeaders(res, path) {
+        if (path.endsWith(".gz")) {
+          res.setHeader("cache-control", "public, max-age=604800, immutable");
+          res.setHeader("content-encoding", "gzip");
+        }
+      },
+    }
+  )
+);
+
+import root from "./route/root.js";
+import serveStaticGZIP from "./middleware/serveStaticGZIP.js";
+app.get("/", root);
+
+app.all("*", (req, res) => {
+  res.sendFile(
+    path.join(fileURLToPath(import.meta.url), "../view/notFound.html")
+  );
 });
 
-app.listen(process.env.port || 8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log(`App listening on port ${process.env.port || 8080}`);
 });
