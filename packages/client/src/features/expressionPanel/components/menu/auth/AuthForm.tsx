3e262539b9ebc4ad964c18529288e6a5553da8dc
David <daviodiosub09@gmail.com>
2025-04-05 12:02:43 +0300

    make forms look good

--- packages/client/src/features/expressionPanel/components/menu/auth/AuthForm.tsx
+++ packages/client/src/features/expressionPanel/components/menu/auth/AuthForm.tsx
@@ -1,4 +1,4 @@
-import React, { useId, useState } from "react";
+import React, { SetStateAction, useId, useState } from "react";
 import FormInput from "../../../../../components/input/FormInput";
 import FilledButton from "../../../../../components/buttons/common/FilledButton";
 import { useLazyFetch } from "../../../../../hooks/api";
@@ -6,6 +6,8 @@ import { authenticateUser } from "../../../../../state/api/actions";
 import ButtonTarget from "../../../../../components/buttons/target/ButtonTarget";
 import { ArrowLeft } from "../../../../../components/svgs";
 import PasswordInput from "../../../../../components/input/PasswordInput";
+import { CSS_VARIABLES } from "../../../../../data/css/variables";
+import Spinner from "../../../../../components/Loading/Spinner/Spinner";
 
 type AuthFormProps = {
   email: string;
@@ -18,14 +20,7 @@ const AuthForm = ({
   handlePreviousStep,
   handleSuccess,
 }: AuthFormProps) => {
-  const id = useId();
   const [password, setPassword] = useState<string>("");
-  const [errorMessage, setErrorMessage] = useState<string | undefined>(
-    undefined
-  );
-  const [trigger, { data, isLoading }] = useLazyFetch(() =>
-    authenticateUser({ email, password })
-  );
 
   return (
     <div className="auth-form">
@@ -35,38 +30,87 @@ const AuthForm = ({
         </ButtonTarget>
         <h2>Welcome back!</h2>
       </div>
-      <form
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-            e.stopPropagation();
-          }
-        }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (isLoading) return;
-          trigger();
-        }}
-      >
-        <label htmlFor={id}>Enter your password:</label>
-        <div style={{ display: "flex", gap: "0.5rem" }}>
-          <PasswordInput
-            name="password"
-            autoFocus={true}
-            id={id}
-            minLength={8}
-            value={password}
-            message={errorMessage}
-            onChange={(e) => {
-              if (errorMessage) setErrorMessage(undefined);
-              setPassword(e.target.value);
-            }}
-          />
-          <FilledButton disabled={password.length < 8}>Submit</FilledButton>
+
+      <div className="auth-form-body">
+        <div className="auth-form-body-content">
+          <p>
+            You're logging in with <strong>{email}</strong>
+          </p>
+          <Form email={email} password={password} setPassword={setPassword} />
         </div>
-      </form>
+      </div>
     </div>
   );
 };
 
 export default AuthForm;
+
+function Form({
+  email,
+  password,
+  setPassword,
+}: {
+  email: string;
+  password: string;
+  setPassword: React.Dispatch<SetStateAction<string>>;
+}) {
+  const id = useId();
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(
+    undefined
+  );
+  const [trigger, { data, isLoading }] = useLazyFetch(() =>
+    authenticateUser({ email, password })
+  );
+
+  return (
+    <form
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (isLoading) return;
+        trigger();
+      }}
+    >
+      <label htmlFor={id}>Enter your password:</label>
+      <div style={{ display: "flex", gap: "0.5rem" }}>
+        <PasswordInput
+          name="password"
+          autoFocus={true}
+          id={id}
+          minLength={8}
+          value={password}
+          message={errorMessage}
+          onChange={(e) => {
+            if (errorMessage) setErrorMessage(undefined);
+            setPassword(e.target.value);
+          }}
+        />
+        <FilledButton disabled={password.length < 8}>
+          {isLoading ? (
+            <div
+              style={{
+                display: "grid",
+                placeContent: "center",
+                width: "2.825rem",
+              }}
+            >
+              <Spinner
+                style={{
+                  borderColor: CSS_VARIABLES.onPrimary,
+                  borderTopColor: "transparent",
+                }}
+              />
+            </div>
+          ) : (
+            "Submit"
+          )}
+        </FilledButton>
+      </div>
+    </form>
+  );
+}
--- packages/client/src/features/expressionPanel/components/menu/auth/RegisterForm.tsx
+++ packages/client/src/features/expressionPanel/components/menu/auth/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { useId, useState } from "react";
+import React, { SetStateAction, useId, useState } from "react";
 import ButtonTarget from "../../../../../components/buttons/target/ButtonTarget";
 import { ArrowLeft } from "../../../../../components/svgs";
 import { RegisterUserData } from "../../../../../state/api/types";
@@ -8,6 +8,7 @@ import { useLazyFetch } from "../../../../../hooks/api";
 import { registerUser } from "../../../../../state/api/actions";
 import Spinner from "../../../../../components/Loading/Spinner/Spinner";
 import PasswordInput from "../../../../../components/input/PasswordInput";
+import { CSS_VARIABLES } from "../../../../../data/css/variables";
 
 type RegisterFormProps = {
   credentials: RegisterUserData;
@@ -22,12 +23,6 @@ const RegisterForm = ({
   handlePreviousStep,
 }: RegisterFormProps) => {
   const [userData, setUserData] = useState<RegisterUserData>(credentials);
-  const firstNameId = useId();
-  const lastNameId = useId();
-  const passwordId = useId();
-  const [trigger, { data, isLoading }] = useLazyFetch(() =>
-    registerUser(userData)
-  );
 
   return (
     <div className="register-form">
@@ -44,78 +39,121 @@ const RegisterForm = ({
         >
           <ArrowLeft />
         </ButtonTarget>
-        <h2>Welcome</h2>
+        <h2>Welcome!</h2>
       </div>
 
-      <form
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-            e.stopPropagation();
-          }
-        }}
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (isLoading) return;
-          trigger();
-        }}
-      >
-        <div>
-          <div>
-            <label htmlFor={firstNameId}>First Name:</label>
-            <FormInput
-              name="firstName"
-              type="text"
-              required
-              id={firstNameId}
-              value={userData.firstName}
-              onChange={(e) => {
-                setUserData({ ...userData, firstName: e.target.value });
-              }}
-            />
-          </div>
-          <div>
-            <label htmlFor={lastNameId}>Last Name (optional):</label>
-            <FormInput
-              name="lastName"
-              type="text"
-              id={lastNameId}
-              value={userData.lastName}
-              onChange={(e) => {
-                setUserData({ ...userData, lastName: e.target.value });
-              }}
-            />
-          </div>
+      <div className="register-form-body">
+        <div className="register-form-body-content">
+          <p>
+            You're Signing in with <strong>{userData.email}.</strong>
+          </p>
+          <Form userData={userData} setUserData={setUserData} />
         </div>
-        <div>
-          <label htmlFor={passwordId}>Password:</label>
-          <PasswordInput
-            name="password"
-            id={passwordId}
-            minLength={8}
-            required
-            value={userData.password}
-            onChange={(e) => {
-              setUserData({ ...userData, password: e.target.value });
-            }}
-          />
-        </div>
-        <div style={{ display: "flex", justifyContent: "flex-end" }}>
-          <FilledButton
-            disabled={userData.password.length < 8 || userData.firstName === ""}
-          >
-            {isLoading ? (
-              <div>
-                <Spinner />
-              </div>
-            ) : (
-              "Submit"
-            )}
-          </FilledButton>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
 
 export default RegisterForm;
+
+function Form({
+  userData,
+  setUserData,
+}: {
+  userData: RegisterUserData;
+  setUserData: React.Dispatch<SetStateAction<RegisterUserData>>;
+}) {
+  const firstNameId = useId();
+  const lastNameId = useId();
+  const passwordId = useId();
+  const [trigger, { data, isLoading }] = useLazyFetch(() =>
+    registerUser(userData)
+  );
+
+  return (
+    <form
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }}
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (isLoading) return;
+        trigger();
+      }}
+    >
+      <div>
+        <div>
+          <label htmlFor={firstNameId}>First Name:</label>
+          <FormInput
+            name="firstName"
+            type="text"
+            required
+            id={firstNameId}
+            value={userData.firstName}
+            onChange={(e) => {
+              setUserData({ ...userData, firstName: e.target.value });
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor={lastNameId}>Last Name (optional):</label>
+          <FormInput
+            name="lastName"
+            type="text"
+            id={lastNameId}
+            value={userData.lastName}
+            onChange={(e) => {
+              setUserData({ ...userData, lastName: e.target.value });
+            }}
+          />
+        </div>
+      </div>
+      <div>
+        <label htmlFor={passwordId}>Password:</label>
+        <PasswordInput
+          name="password"
+          id={passwordId}
+          minLength={8}
+          required
+          value={userData.password}
+          onChange={(e) => {
+            setUserData({ ...userData, password: e.target.value });
+          }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          marginTop: "2rem",
+        }}
+      >
+        <FilledButton
+          disabled={userData.password.length < 8 || userData.firstName === ""}
+        >
+          {isLoading ? (
+            <div
+              style={{
+                display: "grid",
+                placeContent: "center",
+                width: "2.825rem",
+              }}
+            >
+              <Spinner
+                style={{
+                  borderColor: CSS_VARIABLES.onPrimary,
+                  borderTopColor: "transparent",
+                }}
+              />
+            </div>
+          ) : (
+            "Submit"
+          )}
+        </FilledButton>
+      </div>
+    </form>
+  );
+}
